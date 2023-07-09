496989cc91563d71bd289329bea5a28540481e46
Paweł Ciosek <ciosek.pawel@gmail.com>
2023-07-09 10:54:54 +0200

    abstract fabric

--- wzorce_projektowe/strukturalne/fabryka_abstrakcyjna.ts
+++ wzorce_projektowe/strukturalne/fabryka_abstrakcyjna.ts
@@ -1,9 +1,64 @@
+abstract class UIElement {}
+
+// definiujemy abstrakcyjne produkty dla systemu
+abstract class UIButton extends UIElement {}
+abstract class UIInput extends UIElement {}
+
+// abstrakcyjna fabryka
+// deklaruje metody zwracające różne abstrakcyjne produkty ale z jednej rodziny
+// rodzina w wspólny koncept, coś co powoduje że produkty są kompatybilne ze sobą
 abstract class UIElementFactory {
-    protected name: string = 'test';
+    abstract createButton(): UIButton;
+    abstract createInput(): UIInput;
 }
 
-class Dupa extends UIElementFactory {
-    dupaSiie() {
-        return this.name;
+// budujemy konkretne produkty dla rodziny
+class MacOsUIButton extends UIButton {}
+class MacOsUIInput extends UIInput {}
+
+// definiujemy konkretną fabrykę, zwraca produkty konkretnej rodziny
+class MacOsUIElementFactory extends UIElementFactory {
+    createButton(): UIButton {
+        let o = new MacOsUIButton();
+        return o;
+    }
+
+    createInput(): UIInput {
+        let o = new MacOsUIInput();
+        return o;
     }
 }
+
+// analogicznie jak dla macOS mamy rodzinę Windows
+class WindowsUIButton extends UIButton {}
+class WindowsUIInput extends UIInput {}
+
+// analogicznie mamy fabrykę Windows
+class WindowsUIElementFactory extends UIElementFactory {
+    createButton(): UIButton {
+        let o = new WindowsUIButton();
+        return o;
+    }
+
+    createInput(): UIInput {
+        let o = new WindowsUIInput();
+        return o;
+    }
+}
+
+// ... i tak dalej, możemy definiować kolejne fabryki abstrakcyjne
+
+let f: UIElementFactory; // mamy w systemie fabrykę
+
+let config: "macos" | "windows";
+
+// element konfigutacji sytemu, wybieramy fabrykę w zależności od parametru
+config = "macos";
+f = new MacOsUIElementFactory();
+if (config.toString() == "windows") {
+    f = new WindowsUIElementFactory();
+}
+
+// dalszy element aplikacji, niezależenie jaka jest fabryka to zadziałą i pozostaje niezmienne
+let button: UIButton = f.createButton();
+let input: UIInput = f.createInput();
